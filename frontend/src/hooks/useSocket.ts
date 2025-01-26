e278e2b1496b2b522710169ac3cbbf2efc14815e
Hasnain Ahamed Nisan <68366713+hasnain-nisan@users.noreply.github.com>
2025-01-26 14:59:00 +0600

    debug socket connection

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -3,6 +3,9 @@ import { io, Socket } from 'socket.io-client';
 
 const SOCKET_URL = import.meta.env.VITE_API_BASE_URL;
 
+console.log({SOCKET_URL});
+
+
 export const useSocket = (event: string) => {
    const [data, setData] = useState<any[]>([]);
 
